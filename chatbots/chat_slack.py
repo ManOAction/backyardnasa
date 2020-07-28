16d1e8b8fa317ebbb583bc1f662cc037cded696a
ManOAction <7423159+ManOAction@users.noreply.github.com>
2020-07-27 23:32:03 -0600

    ramblings about webhooks.

--- chatbots/chat_slack.py
+++ chatbots/chat_slack.py
@@ -1,85 +1,79 @@
-"""Draft of our Slack Chatbot."""
+"""Draft of our Slack Chatbot.
+
+# Diary
+
+7/27 - This Watson linked Chatbot looks like exactly what we need.
+https://api.slack.com/tutorials/watson-sentiment
+Maybe this too for learning about webhooks.  This seems like an easier solution.
+https://blog.bearer.sh/consume-webhooks-with-python/
+
+7/27 - Basic auth is in place and we can chat to a channel.  Need to figure out the
+Events api so we can get notified.  I've updated secrets.
+
+7/27 - We rebuit the app and added scopes.  They gave us a fun bot user token
+and we put it in secrets.
+
+7/27 - We're hung up Oath2 vs. old tokens which is the legacy system.  We're going to
+backup and start from scratch because they've got a legacy systm that we're mixed up
+with.  Working from here: https://api.slack.com/authentication/basics
+
+
+
+"""
+# Imports
+################################################################################
 import json
 import requests
 import re
-import twit_secrets as secrets
+import toml
 
+# Constants
+################################################################################
+cnf = toml.load('secrets.toml')
+token = cnf['SlackSecrets']['BotOathToken']
 
+channel = 'CUCGQMTHN'
+message = 'I\'m alive! Hello world.'
 
+# Functions
+################################################################################
 
-
-
-################ Post using chat api
-# headers = {
-#     'Authorization': 'Bearer {0}'.format(secrets.SlackToken),
-#     'Content-type': 'application/json',
-# }
-#
-# data = """{
-#     "channel":"C061EG9SL",
-#     "text":"Hello World, again." #,
-#     # "attachments": [{"text":"Who wins the lifetime supply of chocolate?",
-#     #                 "fallback":"You could be telling the computer exactly what it can do with a lifetime supply of chocolate.",
-#     #                 "color":"#3AA3E3",
-#     #                 "attachment_type":"default","callback_id":"select_simple_1234",
-#     #                 "actions":[{"name":"winners_list","text":"Who should win?","type":"select","data_source":"users"}]}]
-#     }'
-# """
-# response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, data=data)
-
-
-# ###############
-# headers = {
-#     'Content-type': 'application/x-www-form-urlencoded',
-#     'Authorization': 'Bearer {0}'.format(secrets.SlackToken)
-# }
-#
-# data = """{
-#     "channel":"#matilda-talk",
-#     "text":"Yeah, I can talk now."}
-#     """
-#
-# response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, data=data)
-#
-# print(response.content)
-# MatildaName = re.compile('<@UTZNYQU9G>')
-
-
-##############################################
-def get_most_recent_message():
-    headers = {
-        'Content-type': 'application/x-www-form-urlencoded'
-        }
-
-    MessageText = False
-
-    response = requests.get('https://slack.com/api/conversations.history?token={0}&channel=CUCGQMTHN&limit=15'.format(secrets.SlackToken), headers=headers)
-    response = json.loads(response.content)
-    # print(response)
-    response = response['messages']
-
-    for message in response:
-        if re.match(r"<@UTZNYQU9G>", message['text']):
-            MessageText = print(message['text'])
-            return MessageText
-
-    return MessageText
-
-
-def post_to_channel(message):
-    headers = {
-        'Content-type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer {0}'.format(secrets.SlackToken)
+def print_channels(f_token):
+    files = {
+             'token': (None, f_token),
             }
-    data = """{
-        "channel":"#matilda-talk",
-        "text":"{0}".}
-        """.format(message)
-    response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, data=data)
+
+    response = requests.post('https://slack.com/api/conversations.list', files=files)
+    parsed_resp = json.loads(response.content)
+
+    for channel in parsed_resp['channels']:
+        print(channel['id'], " ", channel['name'])
+
+    # print(json.dumps(parsed_resp, indent=4, sort_keys=True))
 
     return True
 
-##############################################
+
+def post_to_channel(f_token, f_channel, f_message):
+    files = {
+        'token': (None, f_token),
+        'channel': (None, f_channel),
+        'text': (None, f_message),
+    }
+
+    response = requests.post('https://slack.com/api/chat.postMessage', files=files)
+
+    print(response)
+    print(response.content)
+
+    return True
 
 
-while __name__ == "__main__":
+# Run Script
+################################################################################
+
+print_channels(token)
+
+# post_to_channel(token, channel, message)
+
+print('End Of File')
