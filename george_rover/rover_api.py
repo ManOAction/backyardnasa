536b5b4e65779a0622aa79fc015484690efd87f8
ManOAction <7423159+ManOAction@users.noreply.github.com>
2023-11-17 17:24:11 -0700

    updates

--- george_rover/rover_api.py
+++ george_rover/rover_api.py
@@ -1,14 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from icecream import ic
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    ic(request)
     if request.method == "POST":
-        print("Data received from Webhook is: ", request.json)
-        # Create a response message
         return jsonify(message="Webhook received! Here's your response.")
 
+    else:
+        return jsonify(message="Method not allowed."), 405
 
-app.run(host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
